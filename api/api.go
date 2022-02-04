cf00b7b195be5d699df9f4f1b3a7708183fe533a
Pratik <mota.pratik@gmail.com>
2022-02-04 21:21:54 +0530

    initial checkin

---
+++ api/api.go
@@ -0,0 +1,60 @@
+package api
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"sort"
+	"strings"
+)
+
+type InputText struct {
+	Text string `json:"text"`
+}
+
+type WordCount struct {
+	Key   string `json:"word"`
+	Value int    `json:"count"`
+}
+
+func TopTenWordCount(w http.ResponseWriter, r *http.Request) {
+
+	var inputTextRequest InputText
+	err := json.NewDecoder(r.Body).Decode(&inputTextRequest)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+	// Split Words by space
+	textArray := strings.Split(inputTextRequest.Text, " ")
+	wordOccurrenceMap := make(map[string]int)
+	for _, text := range textArray {
+		wordOccurrenceMap[text] += 1
+	}
+	// Sort Map Key-Value Pair
+	occurenceWordMap := SortMapByValue(wordOccurrenceMap)
+
+	// Create JSON of Map
+	w.WriteHeader(http.StatusOK)
+	errMsg := json.NewEncoder(w).Encode(occurenceWordMap)
+	if errMsg != nil {
+		log.Println("Error in API")
+	}
+}
+
+func SortMapByValue(wordFrequencyMap map[string]int) []WordCount {
+
+	var wordsCounts []WordCount
+	for key, value := range wordFrequencyMap {
+		wordsCounts = append(wordsCounts, WordCount{Key: key, Value: value})
+	}
+
+	sort.Slice(wordsCounts, func(i, j int) bool {
+		return wordsCounts[i].Value > wordsCounts[j].Value
+	})
+
+	if len(wordsCounts) > 10 {
+		return wordsCounts[:10]
+	}
+
+	return wordsCounts
+}
