8dae05f7f93537f5758f65daac1a05781cbbb621
Pratik <mota.pratik@gmail.com>
2022-02-04 21:26:25 +0530

    remove comment

--- main.go
+++ main.go
@@ -15,37 +15,3 @@ func main() {
 	//start and listen to requests
 	http.ListenAndServe(":8080", router)
 }
-
-/*
-	//s := []string{"this is my is is my wow so is is my my so hey no yes"}
-	s := []string{"Create to a project that calls service created above, pass text from input file ‘GoLang_Test.txt’ and prints JSONoutput returned from the service. Program should be optimized to process large text file.This task should be completed using GoLang and uploaded to GitHub"}
-	words := strings.Split(strings.Join(s, ""), " ")
-
-	// count same words in s
-	m := make(map[string]int)
-	for _, word := range words {
-		if _, ok := m[word]; ok {
-			m[word]++
-		} else {
-			m[word] = 1
-		}
-	}
-
-	// create and fill slice of word-count pairs for sorting by count
-	wordCounts := make([]WordCount, 0, len(m))
-	for key, val := range m {
-		wordCounts = append(wordCounts, WordCount{word: key, count: val})
-	}
-
-	// sort wordCount slice by decreasing count number
-	sort.Slice(wordCounts, func(i, j int) bool {
-		return wordCounts[i].count > wordCounts[j].count
-	})
-
-	// display the three most frequent words
-	for i := 0; i < len(wordCounts) && i < 10; i++ {
-		fmt.Println(wordCounts[i].word, ":", wordCounts[i].count)
-	}
-
-	w.WriteHeader(http.StatusOK)
-*/
